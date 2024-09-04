120a568c70525af83a3703cb42857c07c8357cc5
andika febriansyah <82563655+afebrii@users.noreply.github.com>
2024-09-04 22:32:37 +0700

    salah

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class SevenDays extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SecondSplash(),
     );
   }
